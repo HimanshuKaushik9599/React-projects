9eb66d78fd3e1b7ae05091917220f580df57df87
Himanshukaushik9599 <himanshukaushik9599@gmail.com>
2023-09-05 23:21:34 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import About from './Components/About';
 import { Navbar } from './Components/Navbar';
 import {Textfile} from './Components/Textfile';
 function App() {
   return (
   <>
  <Navbar title="TextUtils" about="About us"/> 
-<Textfile heading="Enter the text below"/>
+{/* <Textfile heading="Enter the text below"/> */}
+<About/>
   </>
   );
 }
